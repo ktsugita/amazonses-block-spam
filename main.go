b5141f9e458e36c5667bb58b844303a86f7d8e9d
ktsugita <ktsugita@gmail.com>
2024-02-05 14:14:34 +0900

    ログのメッセージを修正

--- main.go
+++ main.go
@@ -11,17 +11,17 @@ import (
 func handler(ctx context.Context, sesEvent events.SimpleEmailEvent) (events.SimpleEmailDisposition, error) {
 	for _, record := range sesEvent.Records {
 		ses := record.SES
-		slog.Info("Hello, World!", "ses.Receipt", ses.Receipt)
+		slog.Info("amazonses-block-spam", "ses.Receipt", ses.Receipt)
 		if ses.Receipt.VirusVerdict.Status == "FAIL" {
-			slog.Info("Hello, World!", "ses.Receipt.VirusVerdict.Status", ses.Receipt.VirusVerdict.Status)
+			slog.Info("amazonses-block-spam", "ses.Receipt.VirusVerdict.Status", ses.Receipt.VirusVerdict.Status)
 			return events.SimpleEmailDisposition{Disposition: events.SimpleEmailContinue}, nil
 		}
 		if ses.Receipt.SpamVerdict.Status == "FAIL" {
-			slog.Info("Hello, World!", "ses.Receipt.SpamVerdict.Status", ses.Receipt.SpamVerdict.Status)
+			slog.Info("amazonses-block-spam", "ses.Receipt.SpamVerdict.Status", ses.Receipt.SpamVerdict.Status)
 			return events.SimpleEmailDisposition{Disposition: events.SimpleEmailContinue}, nil
 		}
 		if ses.Receipt.VirusVerdict.Status == "FAIL" {
-			slog.Info("Hello, World!", "ses.Receipt.DMARCVerdict.Status", ses.Receipt.DMARCVerdict.Status)
+			slog.Info("amazonses-block-spam", "ses.Receipt.DMARCVerdict.Status", ses.Receipt.DMARCVerdict.Status)
 			return events.SimpleEmailDisposition{Disposition: events.SimpleEmailContinue}, nil
 		}
 	}
